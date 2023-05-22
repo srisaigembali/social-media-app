709e6da01f3b4a5f307b32fb8243577491ad82de
srisaigembali <70004293+srisaigembali@users.noreply.github.com>
2023-05-22 07:46:13 +0530

    added theme, styling and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,31 @@
+import HomePage from 'pages/HomePage';
+import LoginPage from 'pages/LoginPage';
+import ProfilePage from 'pages/ProfilePage';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from './theme';
+
 function App() {
-  return <div className="app"></div>;
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import Navbar from './Navbar';
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
+};
+
+export default HomePage;
