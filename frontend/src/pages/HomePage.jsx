74c131c107c52ebf4a0bda679f3e26e454fa623d
srisaigembali <70004293+srisaigembali@users.noreply.github.com>
2023-05-22 12:22:21 +0530

    added posts, post widgets and profile page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,10 @@ import { Box, useMediaQuery } from '@mui/material';
 import { useSelector } from 'react-redux';
 import UserWidget from './widgets/UserWidget';
 import Navbar from './Navbar';
+import MyPostWidget from './widgets/MyPostWidget';
+import PostsWidget from './widgets/PostsWidget';
+import AdvertWidget from './widgets/AdvertWidget';
+import FriendListWidget from './widgets/FriendListWidget';
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery('(min-width:1000px)');
@@ -23,10 +27,15 @@ const HomePage = () => {
         <Box
           flexBasis={isNonMobileScreens ? '42%' : undefined}
           mt={isNonMobileScreens ? undefined : '2rem'}
-        ></Box>
+        >
+          <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
+        </Box>
         {isNonMobileScreens && (
           <Box flexBasis="26%">
+            <AdvertWidget />
             <Box m="2rem 0" />
+            <FriendListWidget userId={_id} />
           </Box>
         )}
       </Box>
